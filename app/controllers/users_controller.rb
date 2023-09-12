323f6a0494625ef1a7aea25869144818b545a184
mee-0389 <139702797+mee-0389@users.noreply.github.com>
2023-09-12 02:34:35 +0000

    [Update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,48 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit]
+
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
   def show
+    @book = Book.new
+    @books = Book.where(user_id:params[:id])
+    @user = User.find(params[:id])
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+    redirect_to books_path
+    end
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.update(user_params)
+      flash[:notice] =  "You have updated user successfully."
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
-  get 'homes/about'
   devise_for :users
+  resources :users, only: [:show, :edit, :update, :index]
+  resources :books
+  root to: 'homes#top'
+  get 'home/about'=> 'homes#about', as:'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
